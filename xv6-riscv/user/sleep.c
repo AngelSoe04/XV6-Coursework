beb8b0b252acad0edb42c726e618abf5e90ac93f
pgzx0354 <pgzx0354@leeds.ac.uk>
2025-10-14 11:45:50 +0000

    Week 3  Chap 4 exercise Done

--- xv6-riscv/user/sleep.c
+++ xv6-riscv/user/sleep.c
@@ -1,19 +1,19 @@
-#include "kernel/sysproc.c"
+#include "kernel/types.h"
 #include "user/user.h"
-#include "kernel/stats.h"
 
-int
-main(int argc, char *argv[])
+
+int main(int argc, char *argv[])
 {
-  int i;
-
-  for(i = 1; i < argc; i++){
-    write(1, argv[i], strlen(argv[i]));
-    if(i + 1 < argc){
-      write(1, " ", 1);
-    } else {
-      write(1, "\n", 1);
-    }
+  if (argc != 2) {
+    fprintf(2, "Usage: sleep ticks\n");
+    exit(1);
   }
+  int ticks = atoi(argv[1]);
+  if (ticks <= 0){
+    fprintf(2, "sleep: invalid number of ticks!\n");
+    exit(1);
+  }
+  pause(ticks);
   exit(0);
 }
+
